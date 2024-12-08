6e9ca073e4f5c0e935de892acf67729d6a572808
xy-man <as4102@fayoum.edu.eg>
2024-12-08 22:50:30 +0200

    initialized project constansts inside const.py

---
+++ src/const.py
@@ -0,0 +1,13 @@
+"""
+This file will contain all project constants (window dimensions, rows, cols, ...etc)
+"""
+
+# Game Window Dimensions
+WIDTH = 700
+HEIGHT = 700
+
+# Board Dimensions
+ROWS = 8
+COLS = 8
+
+SQUARE_SIZE = HEIGHT // ROWS
